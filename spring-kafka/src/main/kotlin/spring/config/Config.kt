4aebc873f059eecbf3b32608922fd33ab325464e
sylhare <sylhare@outlook.com>
2020-07-15 22:27:33 -0400

    Update config

--- spring-kafka/src/main/kotlin/spring/config/Config.kt
+++ spring-kafka/src/main/kotlin/spring/config/Config.kt
@@ -1,4 +1,4 @@
-package spring
+package spring.config
 
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerConfig
@@ -10,27 +10,14 @@ import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
-import org.springframework.kafka.core.*
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
 import org.springframework.kafka.support.serializer.JsonDeserializer
 import org.springframework.kafka.support.serializer.JsonSerializer
 import spring.entity.Foo
 import spring.entity.Foo.Companion.invalid
-
-
-class DiscardJsonDeserializer<T> : JsonDeserializer<Foo>() {
-    private fun tryDeserialize(lambdaDeserialize: () -> Foo, data: ByteArray?) = try {
-        lambdaDeserialize()
-    } catch (e: Exception) {
-        println("Invalid Request ${data?.let { String(it) }} ${e.message}")
-        invalid
-    }
-
-    override fun deserialize(topic: String?, headers: Headers?, data: ByteArray?) =
-        tryDeserialize({ super.deserialize(topic, headers, data) }, data)
-
-    override fun deserialize(topic: String?, data: ByteArray?) =
-        tryDeserialize({ super.deserialize(topic, data) }, data)
-}
 
 @Configuration
 open class Config {
@@ -38,8 +25,7 @@ open class Config {
     @Autowired
     private lateinit var kafkaProperties: KafkaProperties
 
-    @Bean
-    open fun consumerConfigs(): MutableMap<String, Any> {
+    private fun consumerConfigs(): MutableMap<String, Any> {
         val props: MutableMap<String, Any> = kafkaProperties.buildConsumerProperties()
         props[JsonSerializer.ADD_TYPE_INFO_HEADERS] = false
         props[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
@@ -47,16 +33,12 @@ open class Config {
         return props
     }
 
-    @Bean
-    open fun consumerFactory(): ConsumerFactory<String, Foo> {
-        return DefaultKafkaConsumerFactory(
-            consumerConfigs(),
-            StringDeserializer(),
+    private fun consumerFactory() =
+        DefaultKafkaConsumerFactory(consumerConfigs(), StringDeserializer(),
             DiscardJsonDeserializer<Foo>()
         )
-    }
 
-    @Bean
+    @Bean(name=["fooKafkaListenerContainerFactory"])
     open fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, Foo>? {
         val factory: ConcurrentKafkaListenerContainerFactory<String, Foo> = ConcurrentKafkaListenerContainerFactory()
         factory.consumerFactory = consumerFactory()
