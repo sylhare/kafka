4b9b1b1c29a9c71a4879966225450677da352411
sylhare <sylhare@outlook.com>
2023-10-17 15:07:29 -0400

    Update format

--- challenge/src/main/kotlin/avro/AvroProducer.kt
+++ challenge/src/main/kotlin/avro/AvroProducer.kt
@@ -11,22 +11,23 @@ import java.util.*
 
 @Throws(MqttException::class)
 fun main(args: Array<String>) {
-  println("*** Starting VP Producer ***")
-  val producer: KafkaProducer<PositionKey, PositionValue> = kafkaAvroProducer()
-  val subscriber = AvroSubscriber(producer)
-  subscriber.start()
+    println("*** Starting VP Producer ***")
+    val producer: KafkaProducer<PositionKey, PositionValue> = kafkaAvroProducer()
     Runtime.getRuntime().addShutdownHook(Thread {
         println("### Stopping VP Producer ###")
         producer.close()
     })
+    val subscriber = AvroSubscriber(producer)
+    subscriber.start()
 }
 
 private fun kafkaAvroProducer(): KafkaProducer<PositionKey, PositionValue> {
-  val settings = Properties()
-  settings[ProducerConfig.CLIENT_ID_CONFIG] = "vp-producer-avro"
-  settings[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = "kafka:9092"
-  settings[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = KafkaAvroSerializer::class.java
-  settings[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = KafkaAvroSerializer::class.java
-  settings[KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG] = "http://schema-registry:8081"
-  return KafkaProducer(settings)
+    val settings = Properties()
+    settings[ProducerConfig.CLIENT_ID_CONFIG] = "vp-producer-avro"
+    settings[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = "kafka:9092"
+    settings[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = KafkaAvroSerializer::class.java
+    settings[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = KafkaAvroSerializer::class.java
+    settings[KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG] = "http://schema-registry:8081"
+    settings[KafkaAvroSerializerConfig.VALUE_SUBJECT_NAME_STRATEGY] = TopicRecordNameStrategy::class.java
+    return KafkaProducer(settings)
 }
